02fe74bfba636c9864cbea92111ea5aa1a1464cf
cpuCode <923992029@qq.com>
2022-03-01 09:26:32 +0800

    赋值运算符

---
+++ operation/assignOper/assignOperTest.py
@@ -0,0 +1,32 @@
+"""
+ @dauthor : cpucode
+ @date : 2022/3/1 8:50
+ @github : https://github.com/CPU-Code
+ @csdn : https://blog.csdn.net/qq_44226094
+"""
+
+
+a = 22
+b = 11
+c = 0
+
+c = a + b
+print("a + b = ", c)
+
+c += a
+print("c += a = ", c)
+
+c *= a
+print("c *= a = ", c)
+
+c /= a
+print("c /= a = ", c)
+
+c = 2
+print("cc = 2 = ", c)
+
+c **= a
+print("c **= a = ", c)
+
+c //= a
+print("c //= a = ", c)
