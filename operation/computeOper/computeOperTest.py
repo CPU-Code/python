a06551c2276e9fb337a511638be8f655829d9a87
cpuCode <923992029@qq.com>
2022-03-01 09:23:51 +0800

    算术运算符

---
+++ operation/computeOper/computeOperTest.py
@@ -0,0 +1,39 @@
+"""
+ @dauthor : cpucode
+ @date : 2022/2/28 15:11
+ @github : https://github.com/CPU-Code
+ @csdn : https://blog.csdn.net/qq_44226094
+"""
+
+a = 22
+b = 11
+c = 0
+
+c = a + b
+print("a + b = ", c)
+
+c = a - b
+print("a - b = ", c)
+
+c = a * b
+print("a * b = ", c)
+
+c = a / b
+print("a / b = ", c)
+
+c = a % b
+print("a % b = ", c)
+
+
+# 修改变量 a 、b 、c
+a = 2
+b = 3
+c = a**b
+print("a**b = ", c)
+
+
+a = 10
+b = 5
+c = a//b
+print("a//b = ", c)
+
