f8e9e0d5d39df5caa5710cf225fc0194352fdb1d
cpuCode <923992029@qq.com>
2022-03-01 09:26:40 +0800

    位运算符

---
+++ operation/positionOper/positionOperTest.py
@@ -0,0 +1,30 @@
+"""
+ @dauthor : cpucode
+ @date : 2022/3/1 8:54
+ @github : https://github.com/CPU-Code
+ @csdn : https://blog.csdn.net/qq_44226094
+"""
+
+
+a = 22
+b = 11
+c = 0
+
+c = a & b
+print("a & b = ", c)
+
+c = a | b
+print("a | b = ", c)
+
+c = a ^ b
+print("a ^ b = ", c)
+
+c = ~a
+print("~a = ", c)
+
+c = a << 2
+print("a << 2 = ", c)
+
+c = a >> 2
+print("a >> 2 = ", c)
+
