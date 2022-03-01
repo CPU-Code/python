f1be7dda4156421db22c5d933dcce0c6c13d5a4f
cpuCode <923992029@qq.com>
2022-03-01 09:26:27 +0800

    比较运算符

---
+++ operation/compareOper/compareOperTest.py
@@ -0,0 +1,67 @@
+"""
+ @dauthor : cpucode
+ @date : 2022/2/28 15:20
+ @github : https://github.com/CPU-Code
+ @csdn : https://blog.csdn.net/qq_44226094
+"""
+
+
+a = 22
+b = 11
+c = 0
+
+if (a == b):
+    print("a 等于 b")
+else:
+    print("a 不等于 b")
+
+print("/***************************************/")
+
+
+if (a != b):
+    print("a 不等于 b")
+else:
+    print(" a 等于 b")
+
+print("/***************************************/")
+
+
+if (a < b):
+    print("a 小于 b")
+else:
+    print("a 大于等于 b")
+
+print("/***************************************/")
+
+
+if (a > b):
+    print("a 大于 b")
+else:
+    print("a 小于等于 b")
+
+print("/***************************************/")
+
+
+# 修改变量 a 和 b 的值
+a = 5
+b = 20
+
+if (a <= b):
+    print("a 小于等于 b")
+else:
+    print("a 大于  b")
+
+print("/***************************************/")
+
+
+if (b >= a):
+    print("b 大于等于 a")
+else:
+    print("b 小于 a")
+
+
+
+
+
+
+
